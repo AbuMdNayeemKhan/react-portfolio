0da857584ea38d88aaed307cceaf9d1713fc77b0
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-04-14 02:11:02 +0600

    counter section done

--- src/Components/Counter/Counter.jsx
+++ src/Components/Counter/Counter.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 
 const Counter = () => {
     return (
-        <div className='container mx-auto'>
+        <div className='flex justify-around my-9 container mx-auto'>
             <div>
-                <h2>10+</h2>
-                <h4>Years of experience</h4>
+                <h2 className='text-4xl font-bold text-center'>10+</h2>
+                <h4 className='text-xl text-center'>Years of experience</h4>
             </div>
             <div>
-                <h2>90+</h2>
-                <h4>Projects Completed</h4>
+                <h2 className='text-4xl font-bold text-center'>90+</h2>
+                <h4 className='text-xl text-center'>Projects Completed</h4>
             </div>
             <div>
-                <h2>15+</h2>
-                <h4>Happy Clients</h4>
+                <h2 className='text-4xl font-bold text-center'>15+</h2>
+                <h4 className='text-xl text-center'>Happy Clients</h4>
             </div>
         </div>
     );
