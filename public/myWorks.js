a578810b4c511309664de54de18da682b14b31af
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-04-15 02:12:52 +0600

    portfolio section added

---
+++ public/myWorks.js
@@ -0,0 +1,40 @@
+import project_1 from '../src/assets/project_1.svg';
+import project_2 from '../src/assets/project_2.svg';
+import project_3 from '../src/assets/project_3.svg';
+import project_4 from '../src/assets/project_4.svg';
+import project_5 from '../src/assets/project_5.svg';
+import project_6 from '../src/assets/project_6.svg';
+ const myWorksData = [
+    {
+        "wNo": "value1",
+        "wName": "value2",
+        "wImg": project_1
+    },
+    {
+        "wNo": "value4",
+        "wName": "value5",
+        "wImg": project_2
+    },
+    {
+        "wNo": "value7",
+        "wName": "value8",
+        "wImg": project_3
+    },
+    {
+        "wNo": "value10",
+        "wName": "value11",
+        "wImg": project_4
+    },
+    {
+        "wNo": "value13",
+        "wName": "value14",
+        "wImg": project_5
+    },
+    {
+        "wNo": "value16",
+        "wName": "value17",
+        "wImg": project_6
+    }
+]
+
+export default myWorksData;
--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import theamPartan from '../../assets/theme_pattern.svg';
+import myWork from "../../../public/myWorks";
 const Portfolio = () => {
     return (
         <div className='container max-auto'>
@@ -7,22 +8,11 @@ const Portfolio = () => {
                 <h1 className='absolute font-semibold top-0 left-[50%] -translate-x-[50%] text-[90px] z-10'>Portfolio</h1>
                 <img className='absolute left-[50%] top-[45px] -z-1' src={theamPartan} alt="" />
             </div>
-            <div className=''>
-                <div className="">
-                    <img src="" alt="" />
-                </div>
-                <div className="">
-                    <img src="" alt="" />
-                </div>
-                <div className="">
-                    <img src="" alt="" />
-                </div>
-                <div className="">
-                    <img src="" alt="" />
-                </div>
-                <div className="">
-                    <img src="" alt="" />
-                </div>
+            {/* console.log(myWork); */}
+            <div className='container grid grid-cols-3 pt-[200px] mx-auto gap-5'>
+                {myWork.map((work, index)=>{
+                    return <img key={index} src={work.wImg} alt="" />
+                })}
             </div>
         </div>
     );
